9b0a08b341a8980e461b4b0c3312f51e54155233
Allen Sun <allensun623@gmail.com>
2024-06-22 10:09:53 -0700

    update font and wording on the welcome

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['Roboto-Flex', '"Roboto"', 'sans-serif'],
+    },
     extend: {
       colors: {
         primary: '#1D466B',
