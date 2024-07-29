58756ddc5c0d9cacde5873b8ee542849df4b4d4b
Allen Sun <allensun623@gmail.com>
2024-07-29 16:24:56 -0700

    add mouse click hint

--- src/components/welcome/WelcomeWorld.jsx
+++ src/components/welcome/WelcomeWorld.jsx
@@ -1,4 +1,5 @@
 import { bottomBannerText } from '@/constants/text';
+import { motion } from 'framer-motion';
 
 export default function WelcomeWorld() {
   return (
@@ -13,6 +14,15 @@ export default function WelcomeWorld() {
           </p>
         </div>
       </div>
+      {/* mouse click animation */}
+      <motion.div
+        className='z-10 fixed bottom-0 left-0 overflow-hidden flex flex-col justify-center items-center w-full h-2/3 pointer-events-none'
+        initial={{ opacity: 0, x: 10, y: 10 }}
+        animate={{ opacity: 1, x: 0, y: 0 }}
+        transition={{ delay: 5 }}
+      >
+        <img src='/public/assets/gif/Scroll Down Gesture.gif' alt='' />
+      </motion.div>
       <div className='z-10 fixed bottom-0 left-0 overflow-hidden flex flex-col justify-center items-center w-full h-1/5 md:h-1/4 space-y-5'>
         <p className='text-white text-lg sm:text-xl md:text-2xl xl:text-4xl text-center font-extralight whitespace-pre-line leading:none md:leading-normal'>
           {bottomBannerText.welcome.memory}
